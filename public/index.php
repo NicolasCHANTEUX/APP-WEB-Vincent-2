f6ef4d2a1c34cd9d363ff449590daa9856fcb6eb
NicolasCHANTEUX <166408814+NicolasCHANTEUX@users.noreply.github.com>
2026-01-09 02:50:17 +0100

    correction 2

--- public/index.php
+++ public/index.php
@@ -22,7 +22,7 @@ if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
 
     exit(1);
 }
-
+ 
 /*
  *---------------------------------------------------------------
  * SET THE CURRENT DIRECTORY
